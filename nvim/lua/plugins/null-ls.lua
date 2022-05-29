4a503210f19c1a08f1ebe6739f7ffb6ddfcdb1ce
Gustavo Rangel <gustavorangel91@gmail.com>
2022-05-29 17:19:28 -0300

    Refactoring.

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,4 +1,4 @@
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 --> There is no plugin linke lspinstaller for linters/formmaters.
 --> You need to install the source's binary (pacman/AUR should have all of them)
@@ -10,42 +10,44 @@ local diagnostics = null_ls.builtins.diagnostics
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 local code_actions = null_ls.builtins.code_actions
 
-null_ls.setup({
-	debug = false,
-	sources = {
-		--> Formatters <--
-		formatting.black,
-		formatting.isort,
-		formatting.shfmt,
-		formatting.stylua,
-		--> formatting.standardrb,
-		formatting.prettier_standard.with({
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"vue",
-				"css",
-				"scss",
-				"less",
-				"html",
-				"json",
-				"jsonc",
-				"yaml",
-				"markdown",
-				"graphql",
-				"handlebars",
-			},
-		}),
-		--> Linters <--
-		diagnostics.shellcheck,
-	},
-	on_attach = function(client, _)
-		if client.resolved_capabilities.document_formatting then
-			-- formatting_seq_sync() formats with all language server (instead of keeping asking which one you wanna use in case you have multiple installed)
-			-- as an alternative, you can choose a source to disable its formatting capability (like I did for tsserver)
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
-		end
-	end,
-})
+null_ls.setup {
+  debug = false,
+  sources = {
+    --> Formatters <--
+    formatting.black,
+    formatting.isort,
+    formatting.shfmt,
+    formatting.stylua.with {
+      indent_width = 2,
+    },
+    --> formatting.standardrb,
+    formatting.prettier_standard.with {
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "css",
+        "scss",
+        "less",
+        "html",
+        "json",
+        "jsonc",
+        "yaml",
+        "markdown",
+        "graphql",
+        "handlebars",
+      },
+    },
+    --> Linters <--
+    diagnostics.shellcheck,
+  },
+  on_attach = function(client, _)
+    if client.resolved_capabilities.document_formatting then
+      -- formatting_seq_sync() formats with all language server (instead of keeping asking which one you wanna use in case you have multiple installed)
+      -- as an alternative, you can choose a source to disable its formatting capability (like I did for tsserver)
+      vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()"
+    end
+  end,
+}
