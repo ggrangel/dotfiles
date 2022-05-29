9ba30be4c995eb96641dff638227edc9fb556b1b
Gustavo Rangel <gustavorangel91@gmail.com>
2022-05-29 17:17:37 -0300

    Change theme to from darkplus to catppuccin.

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,8 +1,11 @@
-vim.cmd([[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]])
+-- vim.cmd([[
+-- try
+--   colorscheme darkplus
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]])
+
+vim.g.catppuccin_flavour = "macchiato" -- macchiato, mocha, dusk
+vim.cmd [[colorscheme catppuccin]]
