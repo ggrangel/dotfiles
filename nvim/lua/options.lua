b0322ebed4f05fa59ed0e810dc107beec5fffa88
Gustavo Rangel <gustavorangel91@gmail.com>
2022-05-29 17:20:20 -0300

    Change identation size.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,45 +1,47 @@
 local options = {
-	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
-	wrap = false, -- do not wrap long text
-	clipboard = "unnamedplus", -- from yank to clipboard and vice versa
-	splitright = true, -- split vertical window to the right of the current window
-	completeopt = { "menuone", "noinsert", "noselect" }, -- for cmp to work fine
-	updatetime = 300, -- faster completion. default is 4000ms
-	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
+  wrap = false, -- do not wrap long text
+  clipboard = "unnamedplus", -- from yank to clipboard and vice versa
+  splitright = true, -- split vertical window to the right of the current window
+  completeopt = { "menuone", "noinsert", "noselect" }, -- for cmp to work fine
+  updatetime = 300, -- faster completion. default is 4000ms
+  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 
-	-- set this to false when running macros with find (/) words
-	wrapscan = true,
+  -- set this to false when running macros with find (/) words
+  wrapscan = true,
 
-	------ Numbering
-	relativenumber = true,
-	number = true,
+  ------ Numbering
+  relativenumber = true,
+  number = true,
 
-	------ Searching
-	hlsearch = false, -- do not highlight search pattern
-	incsearch = true, -- gradually show matched pattern
+  ------ Searching
+  hlsearch = false, -- do not highlight search pattern
+  incsearch = true, -- gradually show matched pattern
 
-	------ Indenting
-	tabstop = 4, -- number of spaces that a <Tab> in the file counts for
-	softtabstop = 4,
-	shiftwidth = 4,
-	expandtab = true,
-	smartindent = true,
+  ------ Indenting
+  tabstop = 2, -- number of spaces that a <Tab> in the file counts for
+  softtabstop = 2,
+  shiftwidth = 2,
+  expandtab = true,
+  smartindent = true,
 
-	------ Smart-diacritic search
-	smartcase = true,
-	ignorecase = true,
+  ------ Smart-diacritic search
+  smartcase = true,
+  ignorecase = true,
 
-	------ Manage files
-	swapfile = false,
-	backup = false,
-	undofile = true, -- enables persistent undo
+  ------ Manage files
+  swapfile = false,
+  backup = false,
+  undofile = true, -- enables persistent undo
 
-	guifont = "monospace:h17", -- the font used in graphical neovim applications
-	-- termguicolors = true -- for colorizer to work
+  guifont = "monospace:h17", -- the font used in graphical neovim applications
+  -- termguicolors = true -- for colorizer to work
+
+  shadafile = "NONE", -- improves startuptime a little bit
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 -- Treesitter folding
