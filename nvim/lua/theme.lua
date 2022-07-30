2f2b90d55f5f44a242922741987309c80acc18d5
Gustavo Rangel <gustavorangel91@gmail.com>
2022-07-30 08:54:29 -0300

    Change theme.

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,11 +1,11 @@
--- vim.cmd [[
--- try
---   colorscheme darkplus
--- catch /^Vim\%((\a\+)\)\=:E185/
---   colorscheme default
---   set background=dark
--- endtry
--- ]]
+vim.cmd [[
+try
+  colorscheme darkplus
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
 
 -- local flavour = "mocha"
 --
