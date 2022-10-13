a18f2c305fb9c409c917161824a56e3be7717160
Gustavo Rangel <gustavorangel91@gmail.com>
2022-10-13 16:30:26 -0300

    add rules to telegram-desktop and brave

--- awesome/setup/rules.lua
+++ awesome/setup/rules.lua
@@ -4,22 +4,36 @@ local keybindings = require("setup/keybindings")
 
 -- Rules to apply to new clients (through the "manage" signal).
 local rules = {
-	-- All clients will match this rule.
-	{
-		rule = {},
-		properties = {
-			border_width = theme.border_width,
-			border_color = theme.border_normal,
-			focus = awful.client.focus.filter,
-			raise = true,
-			keys = keybindings.clientkeys,
-			screen = awful.screen.preferred,
-			placement = awful.placement.no_overlap + awful.placement.no_offscreen,
-			maximized = false,
-		},
-	},
+  -- All clients will match this rule.
+  {
+    rule = {},
+    properties = {
+      border_width = theme.border_width,
+      border_color = theme.border_normal,
+      focus = awful.client.focus.filter,
+      raise = true,
+      keys = keybindings.clientkeys,
+      screen = awful.screen.preferred,
+      placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+      maximized = false,
+    },
+  },
 
-	{ rule = { instance = "Insync" }, properties = { screen = screen[1], tag = "4" } },
+  { rule = { instance = "Insync" }, properties = { screen = screen[1], tag = "4" } },
 }
 
+local file = io.open("/etc/hostname")
+if file ~= nil then
+  local host_name = file:read()
+
+  if host_name == "core" then
+    table.insert(
+      rules,
+      { rule = { instance = "telegram-desktop" }, properties = { screen = screen[3], tag = "4" } }
+    )
+  elseif host_name == "aux" then
+    table.insert(rules, { rule = { instance = "telegram-desktop" }, properties = { tag = "4" } })
+    table.insert(rules, { rule = { instance = "brave-browser" }, properties = { tag = "2" } })
+  end
+end
 return rules
