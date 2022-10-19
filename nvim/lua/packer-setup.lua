fe2515b65d6720e3f448b999394ace6b9fc12992
Gustavo Rangel <gustavorangel91@gmail.com>
2022-10-19 17:34:18 -0300

    add nvim-treesitter-context

--- nvim/lua/packer-setup.lua
+++ nvim/lua/packer-setup.lua
@@ -68,6 +68,7 @@ return require("packer").startup(function()
   use { "nvim-treesitter/playground" }
   use "RRethy/nvim-treesitter-textsubjects"
   use "nvim-treesitter/nvim-treesitter-textobjects"
+  use "nvim-treesitter/nvim-treesitter-context"
 
   -- Navigation
   use {
