f701d166325ff75777560afba9467887c3a53285
Gustavo Rangel <gustavorangel91@gmail.com>
2022-10-22 10:12:44 -0300

    show all lsp and nullls sserves in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -74,54 +74,6 @@ local function ins_right(component)
   table.insert(config.sections.lualine_x, component)
 end
 
--- ins_left({
--- 	function()
--- 		return "▊"
--- 	end,
--- 	color = { fg = colors.blue }, -- Sets highlighting of component
--- 	padding = { left = 0, right = 1 }, -- We don't need space before this
--- })
-
--- ins_left({
---     -- mode component
---     function()
---         return ""
---     end,
---     color = function()
---         -- auto change color according to neovims mode
---         local mode_color = {
---             n = colors.red,
---             i = colors.green,
---             v = colors.blue,
---             [""] = colors.blue,
---             V = colors.blue,
---             c = colors.magenta,
---             no = colors.red,
---             s = colors.orange,
---             S = colors.orange,
---             [""] = colors.orange,
---             ic = colors.yellow,
---             R = colors.violet,
---             Rv = colors.violet,
---             cv = colors.red,
---             ce = colors.red,
---             r = colors.cyan,
---             rm = colors.cyan,
---             ["r?"] = colors.cyan,
---             ["!"] = colors.red,
---             t = colors.red,
---         }
---         return { fg = mode_color[vim.fn.mode()] }
---     end,
---     padding = { right = 1 },
--- })
-
--- ins_left({
---     -- filesize component
---     "filesize",
---     cond = conditions.buffer_not_empty,
--- })
-
 ins_left {
   "branch",
   color = { fg = colors.violet, gui = "bold" },
@@ -169,36 +121,96 @@ ins_right {
   color = { fg = colors.green, gui = "bold" },
 }
 
-ins_right {
-  -- Lsp server name .
-  function()
-    local msg = "No Active Lsp"
-    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
+local does_table_contain_element = function(table, element)
+  for _, value in ipairs(table) do
+    if value == element then
+      return true
     end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
+  end
+  return false
+end
+
+local null_ls = require "null-ls"
+
+local get_nullls_sources = function(buf_ft)
+  local sources = null_ls.get_sources()
+  local sources_name = {}
+
+  for _, source in pairs(sources) do
+    local filetypes = source.filetypes
+    for ft, active in pairs(filetypes) do
+      if ft == buf_ft and active == true then
+        if not does_table_contain_element(sources_name, source.name) then
+          table.insert(sources_name, source.name)
+        end
       end
     end
+  end
+  return sources_name
+end
+
+local get_all_attached_clients = function(buf_ft)
+  local clients = vim.lsp.get_active_clients()
+
+  if next(clients) == nil then
+    return nil
+  end
+
+  local clients_name = {}
+
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      if client.name == "null-ls" then
+        local nls_sources = get_nullls_sources(buf_ft)
+        for _, source in ipairs(nls_sources) do
+          table.insert(clients_name, source)
+        end
+      else
+        table.insert(clients_name, client.name)
+      end
+    end
+  end
+
+  return clients_name
+end
+
+local build_message = function(clients)
+  local possibly_add_word_separator = function(msg)
+    if msg ~= "" then
+      msg = msg .. ", "
+    end
+    return msg
+  end
+
+  if clients == nil then
+    return "none"
+  end
+
+  local msg = ""
+  for _, server in ipairs(clients) do
+    msg = possibly_add_word_separator(msg)
+    msg = msg .. server
+  end
+
+  return msg
+end
+
+ins_right {
+  function()
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+
+    local clients = get_all_attached_clients(buf_ft)
+
+    local msg = build_message(clients)
+
     return msg
   end,
-  icon = " LSP:",
+  icon = " servers:",
   color = { fg = colors.violet, gui = "bold" },
 }
 
 ins_right { "progress", color = { fg = colors.orange, gui = "bold" } }
 
--- ins_right({
--- 	function()
--- 		return "▊"
--- 	end,
--- 	color = { fg = colors.blue },
--- 	padding = { left = 1 },
--- })
-
 lualine.setup(config)
-lualine.hide() -- hidden by default
+-- lualine.hide() -- hidden by default
