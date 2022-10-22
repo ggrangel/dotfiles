673e2265e13d5e5142f3fcd45b0ae33a129d93a3
Gustavo Rangel <gustavorangel91@gmail.com>
2022-10-22 17:47:51 -0300

    refactoring

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,22 +11,26 @@ vim.g.mapleader = " "
 
 keymap("n", "<leader>A", ":wa | lua vim.notify('Project saved') <CR>")
 keymap("n", "<leader>S", ":wa | source | lua vim.notify('Config sourced') <CR>", { silent = true })
-keymap(
-  "n",
-  "<leader><leader>s",
-  ":source ~/.config/nvim/lua/plugins/luasnip-setup.lua | lua vim.notify('Luasnip sourced') <CR>"
-)
-
-vim.cmd [[let g:winresizer_start_key = '<leader><leader>w']] --> window resize
+keymap("n", "<leader>N", ":Nredir Notifications <CR>") -- opens notification in new window
+vim.cmd([[let g:winresizer_start_key = '<leader>W']]) --> window resize
 
 -- Nvim-tree
 keymap("n", "<leader>t", ":NvimTreeToggle <CR>")
 
+--> LuaSnip <--
+vim.api.nvim_create_user_command("LuaSnipEdit", require("luasnip.loaders.from_lua").edit_snippet_files, {})
+keymap("n", "<leader><CR>", "<cmd>LuaSnipEdit<cr>")
+keymap(
+  "n",
+  "<leader>L",
+  ":source ~/.config/nvim/lua/plugins/luasnip-setup.lua | lua vim.notify('Luasnip sourced') <CR>"
+)
+
 -- Lualine
 keymap("n", "<leader>ls", function()
-  local lualine = require "lualine"
+  local lualine = require("lualine")
   if vim.o.ls == 0 then
-    lualine.hide { unhide = true }
+    lualine.hide({ unhide = true })
   else
     lualine.hide()
   end
@@ -41,13 +45,11 @@ keymap("n", "<leader>ch", function()
   end
 end)
 
-keymap("n", "<leader>rn", ":IncRename ")
-
 -- Mirrors vim-surround keybindings to vim-sandwich
 --> ys, yss, yS, ds, cs, S, dss, css
-vim.cmd [[
+vim.cmd([[
 runtime macros/sandwich/keymap/surround.vim
-]]
+]])
 
 ---- Navigate tabs
 keymap("n", "<C-h>", "<C-w>h")
@@ -97,18 +99,14 @@ keymap("n", "<space>gt", vim.lsp.buf.type_definition)
 -- keymap("n", "gr", vim.lsp.buf.references) -- handled by trouble.nvim
 keymap("n", "<leader>ca", vim.lsp.buf.code_action)
 keymap("n", "[d", function()
-  return vim.diagnostic.goto_prev { border = "rounded" }
+  return vim.diagnostic.goto_prev({ border = "rounded" })
 end)
 keymap("n", "]d", function()
-  return vim.diagnostic.goto_next { border = "rounded" }
+  return vim.diagnostic.goto_next({ border = "rounded" })
 end)
 keymap("n", "gl", function()
-  return vim.diagnostic.open_float { border = "rounded" }
+  return vim.diagnostic.open_float({ border = "rounded" })
 end)
-
---> LuaSnip <--
-vim.api.nvim_create_user_command("LuaSnipEdit", require("luasnip.loaders.from_lua").edit_snippet_files, {})
-keymap("n", "<leader><CR>", "<cmd>LuaSnipEdit<cr>")
 
 -- Harpoon
 keymap("n", "<leader>hr", function()
@@ -135,17 +133,3 @@ keymap({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
 keymap({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
 keymap("n", "<c-n>", "<Plug>(YankyCycleForward)")
 keymap("n", "<c-p>", "<Plug>(YankyCycleBackward)")
-
----- Refactoring nvim
--- Remaps for each of the four refactoring operations currently offered by the plugin
--- keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]])
--- keymap("v", "<leader>rf", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]])
--- keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]])
--- keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]])
--- -- Inline variable can also pick up the identifier currently under the cursor without visual mode
--- keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]])
--- -- keymap("n", "<leader>rp", ":lua require('refactoring').debug.printf({below = true})<CR>")
--- -- Print var: this remap should be made in visual mode
--- keymap("v", "<leader>rp", ":lua require('refactoring').debug.print_var({})<CR>")
--- -- Cleanup function: this remap should be made in normal mode
--- keymap("n", "<leader>rc", ":lua require('refactoring').debug.cleanup({})<CR>")
